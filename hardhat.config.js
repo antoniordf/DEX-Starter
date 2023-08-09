874d94771a7ad07fcc08547c2f13e8d680325e15
Antonio Furtado <antoniordf@hotmail.com>
2023-08-09 14:45:05 +0100

    updated config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.9",
+  networks: {
+    sepolia: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
 };
